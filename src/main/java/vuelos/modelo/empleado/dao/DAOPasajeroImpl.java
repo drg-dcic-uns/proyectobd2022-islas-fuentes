1f1999ee24383df2138eeaf94241b79556605a82
NawlFountains <56007803+NawlFountains@users.noreply.github.com>
2022-11-11 02:28:46 -0300

    Implementa recuperacion de pasajeros

--- src/main/java/vuelos/modelo/empleado/dao/DAOPasajeroImpl.java
+++ src/main/java/vuelos/modelo/empleado/dao/DAOPasajeroImpl.java
@@ -1,11 +1,15 @@
 package vuelos.modelo.empleado.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import vuelos.modelo.empleado.beans.PasajeroBean;
+import vuelos.modelo.empleado.beans.PasajeroBeanImpl;
 import vuelos.modelo.empleado.dao.datosprueba.DAOPasajeroDatosPrueba;
 
 public class DAOPasajeroImpl implements DAOPasajero {
@@ -23,28 +27,39 @@ public class DAOPasajeroImpl implements DAOPasajero {
 
 	@Override
 	public PasajeroBean recuperarPasajero(String tipoDoc, int nroDoc) throws Exception {
-		/**
-		 * TODO (parte 2) Deberá recuperar de la B.D. los datos de un pasajero que tenga
-		 * el tipo de documento y numero pasados como parámetro y devolver los datos en
-		 * un objeto EmpleadoBean. Si no existe el pasajero deberá retornar null y si
-		 * ocurre algun error deberá generar y propagar una excepción.
-		 *
-		 * Nota: para acceder a la B.D. utilice la propiedad "conexion" que ya tiene una
-		 * conexión establecida con el servidor de B.D. (inicializada en el constructor
-		 * DAOPasajeroImpl(...)).
-		 */
-
-		/*
-		 * Datos estáticos de prueba. Quitar y reemplazar por código que recupera los
-		 * datos reales.
-		 */
-		PasajeroBean pasajero = DAOPasajeroDatosPrueba.obtenerPasajero(nroDoc);
-
+		
+		PasajeroBean pasajero = null;
+		String sql = "SELECT * FROM vuelos.pasajeros WHERE doc_tipo = '"+tipoDoc+"' AND doc_nro = "+nroDoc+";";
+		
+		try {
+			Statement select = conexion.createStatement();
+			ResultSet rs = select.executeQuery(sql);
+			
+			rs.next();
+			logger.debug("Se recupero el item con doc_tipo {}, doc_nro {}, apellido {}, nombre {}, direccion {} , telefono {} y nacionalida {}",
+					rs.getString("doc_tipo"), rs.getString("doc_nro"), rs.getString("apellido"), 
+					rs.getString("nombre"), rs.getString("direccion"), rs.getString("telefono"), rs.getString("nacionalidad"));
+			
+			pasajero = new PasajeroBeanImpl();
+			pasajero.setNombre(rs.getString("nombre"));
+			pasajero.setApellido(rs.getString("apellido"));
+			pasajero.setDireccion(rs.getString("direccion"));
+			pasajero.setNroDocumento(rs.getInt("doc_nro"));
+			pasajero.setTipoDocumento(rs.getString("doc_tipo"));
+			pasajero.setTelefono(rs.getString("telefono"));
+			pasajero.setNacionalidad(rs.getString("nacionalidad"));
+			
+		} catch (SQLException ex ) {
+			logger.error("SQLException: " + ex.getMessage());
+			logger.error("SQLState: " + ex.getSQLState());
+			logger.error("VendorError: " + ex.getErrorCode());
+			throw new Exception("Error al buscar pasajero en la B.D.");
+		}
+		
+		
 		logger.info("El DAO retorna al pasajero {} {}", pasajero.getApellido(), pasajero.getNombre());
-
+		
 		return pasajero;
-		// Fin datos estáticos de prueba.
-
 	}
 
 }
